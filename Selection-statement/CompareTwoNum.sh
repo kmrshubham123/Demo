064078d1eaf5f90b98dc1116f30eabdd819bfd27
kmrshubham123 <kmrshubham123@gmail.com>
2021-05-08 18:49:54 +0530

    added

---
+++ Selection-statement/CompareTwoNum.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+num1=10
+num2=10
+ if [$num1 -gt $num2 ]
+then
+
+echo $num1 is greater than $num2
+
+elif [$num1 -eq $ $num2]
+then
+echo $num1 is equal to $num2
+else
+echo $num2 is grater than $num1
+ fi
+
