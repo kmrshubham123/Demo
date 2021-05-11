07814f0ebac473563d4de8791eb17093fb159015
stutisaurya <stuti.saurya@gmail.com>
2021-05-11 18:00:05 +0530

    added

--- While-loop/PrimeFactor.sh
+++ While-loop/PrimeFactor.sh
@@ -4,14 +4,13 @@ if [ $input -lt 1 ];then
 echo "not allowed!"
 exit 1
 fi
-# find factors and prime
 
 i=2
 count=0
 flag=0
 for ((i;i<$input;));do
-    
-  if [ `expr $input % $i` -eq 0 ];then
+
+if [ `expr $input % $i` -eq 0 ];then
       factor=$i
 
   for ((j=2;j<=`expr $factor / 2`;));do
